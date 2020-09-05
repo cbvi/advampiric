cac8ff99838f73be46839d0e20bfaf3836bfb4bb
Carlin Bingham <cb@viennan.net>
2020-09-05 23:51:05 +1200

    more

--- advampiric.adb
+++ advampiric.adb
@@ -2,9 +2,10 @@ with Ada.Text_IO;
 with Ada.Wide_Wide_Text_IO;
 with Ada.Strings.Bounded;
 with Ada.Text_IO.Bounded_IO;
+with Ada.Containers.Indefinite_Vectors;
 
 procedure advampiric is
-   type Name_Access is access constant String;
+   type Name_Access is not null access constant String;
    type Name_List is array (Positive range <>) of Name_Access;
 
    package IO renames Ada.Text_IO;
@@ -15,7 +16,7 @@ procedure advampiric is
    package BIO
       is new Ada.Text_IO.Bounded_IO (Bounded => BS);
 
-   type Log (Important_Count : Natural) is
+   type Log_Type (Important_Count : Natural) is
       record
          Id          : Natural;
          Name        : BS.Bounded_String;
@@ -23,7 +24,11 @@ procedure advampiric is
          Important   : Name_List (1 .. Important_Count);
       end record;
 
-   Dfly : Log := (
+   package Log_Vectors is new Ada.Containers.Indefinite_Vectors
+      (Index_Type => Natural,
+       Element_Type => Log_Type);
+
+   Dfly : Log_Type := (
       Id => 1,
       Name => BS.To_Bounded_String ("log2"),
       Path => BS.To_Bounded_String ("log2.log"),
@@ -34,7 +39,7 @@ procedure advampiric is
       Important_Count => 2
    );
 
-   Myr : Log := (
+   Myr : Log_Type := (
       Id => 2,
       Name => BS.To_Bounded_String ("log1"),
       Path => BS.To_Bounded_String ("log1.log"),
@@ -43,10 +48,17 @@ procedure advampiric is
       ),
       Important_Count => 1
    );
+
+   Logs : Log_Vectors.Vector := Log_Vectors."&"(Dfly, Myr);
+
 begin
    Ada.Wide_Wide_Text_IO.Put_Line ("test");
 
    BIO.Put_Line (Myr.Name);
+
+   for L of Logs loop
+      BIO.Put_Line (L.Path);
+   end loop;
 
    for I in Dfly.Important'Range loop
       IO.Put_Line (Dfly.Important (I).all);
